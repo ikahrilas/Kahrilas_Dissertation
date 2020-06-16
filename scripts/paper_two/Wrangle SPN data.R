fc6430c957562a24b40a1852e550979efb63450c
Ian Kahrilas <ijk60068@gmail.com>
2020-06-16 08:31:56 -0500

    change read_table to read_delim, proper block order for split case.

--- scripts/paper_two/Wrangle SPN data.R
+++ scripts/paper_two/Wrangle SPN data.R
@@ -41,10 +41,10 @@ spn_dat <- files %>%
 # read in evt files
 spn_evt <- files_evt %>%
   map_dfr(~{
-    read_table(.x) %>%
-      separate(`Code	TriNo	Comnt`, into = c("block", "n_trials"), sep = " ") %>%
+    read_delim(.x, "\t", escape_double = FALSE, trim_ws = TRUE) %>%
+      separate(Comnt, into = c("block", "n_trials"), sep = " ") %>%
       mutate(block = str_extract(block, block_names),
-             n_trials = as.numeric(str_extract(n_trials, "[0-9]{2}")),
+             n_trials = as.numeric(n_trials),
              pid = as.numeric(str_extract(.x, "[0-9]+")),
              prop_trials = n_trials / 40)
   })
@@ -53,12 +53,37 @@ spn_evt <- files_evt %>%
 tmp <- bind_rows(read_table2("data/paper_two/SPN mul/206201831_av-export.mul", skip = 1),
           read_table2("data/paper_two/SPN mul/206201831b_av-export.mul", skip = 1)) %>%
   mutate(pid = 206201831,
-         block = rep(block_names, each = nrow(.) / 7),
+         block = rep(c("Pos_Dec",
+                       "Neg_Inc",
+                       "Neu_Watch",
+                       "Pos_Inc",
+                       "Pos_Watch",
+                       "Neg_Dec",
+                       "Neg_Watch"), each = nrow(.) / 7),
          ms = rep(seq(from = -200, to = 2000,
                       by = ((2200 + (2200 / (nrow(.)/7))) / (nrow(.)/7))),
                   times = 7)
   ) %>%
   select(-X74)
+
+# split evt files
+split_case_evt <- paste0("/Users/ian/tmp/Kahrilas_Dissertation/data/paper_two/SPN evt/", str_subset(list.files("/Users/ian/tmp/Kahrilas_Dissertation/data/paper_two/SPN evt/"), "206201831"))
+
+split_case_evt <- bind_rows(read_delim(split_case_evt[1], "\t", escape_double = FALSE, trim_ws = TRUE),
+                            read_delim(split_case_evt[2], "\t", escape_double = FALSE, trim_ws = TRUE)) %>%
+  separate(Comnt, into = c("block", "n_trials"), sep = " ") %>%
+  mutate(block = c("Pos_Dec",
+                   "Neg_Inc",
+                   "Neu_Watch",
+                   "Pos_Inc",
+                   "Pos_Watch",
+                   "Neg_Dec",
+                   "Neg_Watch"),
+         pid = as.numeric(str_extract(split_case_evt[1], "[0-9]+")),
+         n_trials = as.numeric(n_trials),
+         prop_trials = n_trials / 40)
+
+evt <- bind_rows(evt, split_case_evt)
 
 # merge split file with rest of spn data
 spn_dat <- bind_rows(spn_dat, tmp)
@@ -66,7 +91,8 @@ spn_dat <- bind_rows(spn_dat, tmp)
 names(spn_dat) <- gsub("_.*", "", names(spn_dat))
 
 # merge mul and evt files
-spn <- full_join(spn_dat, spn_evt, by = c("pid", "block"))
+spn <- full_join(spn_dat, spn_evt, by = c("pid", "block")) %>%
+  select(-Tmu, -Code, -TriNo)
 
 # write file
 write_csv(spn, here("data", "paper_two", "created_data", "spn.csv"))
