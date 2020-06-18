2f5f10a7d956ce8015c07cc411abea30a1ae3749
Ian Kahrilas <ijk60068@gmail.com>
2020-06-18 12:40:30 -0500

    Fix block names

--- scripts/paper_two/Wrangle SPN data.R
+++ scripts/paper_two/Wrangle SPN data.R
@@ -53,13 +53,13 @@ spn_evt <- files_evt %>%
 tmp <- bind_rows(read_table2("data/paper_two/SPN mul/206201831_av-export.mul", skip = 1),
           read_table2("data/paper_two/SPN mul/206201831b_av-export.mul", skip = 1)) %>%
   mutate(pid = 206201831,
-         block = rep(c("Pos_Dec",
-                       "Neg_Inc",
-                       "Neu_Watch",
-                       "Pos_Inc",
-                       "Pos_Watch",
-                       "Neg_Dec",
-                       "Neg_Watch"), each = nrow(.) / 7),
+         block = rep(c("Pre_Pos_Inc",
+                       "Pre_Pos_Dec",
+                       "Pre_Pos_Watch",
+                       "Pre_Neu_Watch",
+                       "Pre_Neg_Inc",
+                       "Pre_Neg_Dec",
+                       "Pre_Neg_Watch"), each = nrow(.) / 7),
          ms = rep(seq(from = -200, to = 2000,
                       by = ((2200 + (2200 / (nrow(.)/7))) / (nrow(.)/7))),
                   times = 7)
@@ -72,18 +72,18 @@ split_case_evt <- paste0("/Users/ian/tmp/Kahrilas_Dissertation/data/paper_two/SP
 split_case_evt <- bind_rows(read_delim(split_case_evt[1], "\t", escape_double = FALSE, trim_ws = TRUE),
                             read_delim(split_case_evt[2], "\t", escape_double = FALSE, trim_ws = TRUE)) %>%
   separate(Comnt, into = c("block", "n_trials"), sep = " ") %>%
-  mutate(block = c("Pos_Dec",
-                   "Neg_Inc",
-                   "Neu_Watch",
-                   "Pos_Inc",
-                   "Pos_Watch",
-                   "Neg_Dec",
-                   "Neg_Watch"),
+  mutate(block = c("Pre_Pos_Inc",
+                   "Pre_Pos_Dec",
+                   "Pre_Pos_Watch",
+                   "Pre_Neu_Watch",
+                   "Pre_Neg_Inc",
+                   "Pre_Neg_Dec",
+                   "Pre_Neg_Watch"),
          pid = as.numeric(str_extract(split_case_evt[1], "[0-9]+")),
          n_trials = as.numeric(n_trials),
          prop_trials = n_trials / 40)
 
-evt <- bind_rows(evt, split_case_evt)
+evt <- bind_rows(spn_evt, split_case_evt)
 
 # merge split file with rest of spn data
 spn_dat <- bind_rows(spn_dat, tmp)
@@ -91,7 +91,7 @@ spn_dat <- bind_rows(spn_dat, tmp)
 names(spn_dat) <- gsub("_.*", "", names(spn_dat))
 
 # merge mul and evt files
-spn <- full_join(spn_dat, spn_evt, by = c("pid", "block")) %>%
+spn <- full_join(spn_dat, evt, by = c("pid", "block")) %>%
   select(-Tmu, -Code, -TriNo)
 
 # write file
