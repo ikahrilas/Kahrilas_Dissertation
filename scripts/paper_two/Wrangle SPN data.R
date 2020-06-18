434eaabc55b26cd98ec99f9932f52c737cc83387
Ian Kahrilas <ijk60068@gmail.com>
2020-06-18 13:05:04 -0500

    Continue fixing bugs.

--- scripts/paper_two/Wrangle SPN data.R
+++ scripts/paper_two/Wrangle SPN data.R
@@ -53,11 +53,11 @@ spn_evt <- files_evt %>%
 tmp <- bind_rows(read_table2("data/paper_two/SPN mul/206201831_av-export.mul", skip = 1),
           read_table2("data/paper_two/SPN mul/206201831b_av-export.mul", skip = 1)) %>%
   mutate(pid = 206201831,
-         block = rep(c("Pre_Pos_Inc",
-                       "Pre_Pos_Dec",
-                       "Pre_Pos_Watch",
+         block = rep(c("Pre_Pos_Dec",
                        "Pre_Neu_Watch",
                        "Pre_Neg_Inc",
+                       "Pre_Pos_Inc",
+                       "Pre_Pos_Watch",
                        "Pre_Neg_Dec",
                        "Pre_Neg_Watch"), each = nrow(.) / 7),
          ms = rep(seq(from = -200, to = 2000,
@@ -72,11 +72,11 @@ split_case_evt <- paste0("/Users/ian/tmp/Kahrilas_Dissertation/data/paper_two/SP
 split_case_evt <- bind_rows(read_delim(split_case_evt[1], "\t", escape_double = FALSE, trim_ws = TRUE),
                             read_delim(split_case_evt[2], "\t", escape_double = FALSE, trim_ws = TRUE)) %>%
   separate(Comnt, into = c("block", "n_trials"), sep = " ") %>%
-  mutate(block = c("Pre_Pos_Inc",
-                   "Pre_Pos_Dec",
-                   "Pre_Pos_Watch",
+  mutate(block = c("Pre_Pos_Dec",
                    "Pre_Neu_Watch",
                    "Pre_Neg_Inc",
+                   "Pre_Pos_Inc",
+                   "Pre_Pos_Watch",
                    "Pre_Neg_Dec",
                    "Pre_Neg_Watch"),
          pid = as.numeric(str_extract(split_case_evt[1], "[0-9]+")),
@@ -96,4 +96,3 @@ spn <- full_join(spn_dat, evt, by = c("pid", "block")) %>%
 
 # write file
 write_csv(spn, here("data", "paper_two", "created_data", "spn.csv"))
-
