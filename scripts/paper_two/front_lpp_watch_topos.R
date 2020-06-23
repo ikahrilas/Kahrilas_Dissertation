cb952e332f604a76561749fd9b19622f4109042e
Ian Kahrilas <ijk60068@gmail.com>
2020-06-23 13:08:39 -0500

    Separate scripts for generating topos for frontal LPP.

---
+++ scripts/paper_two/front_lpp_watch_topos.R
@@ -0,0 +1,98 @@
+# Watch topos and ERPs for frontal LPP
+
+library(devtools)
+library(eegUtils) # remotes::install_github("craddm/eegUtils")
+library(tidyverse)
+library(here)
+library(patchwork)
+
+# load electrode layout
+elec_loc <- read_csv(here("data", "paper_two", "Equidistant Layout.csv"))
+elec_loc <- elec_loc %>%
+  rename("channel" = `channel name`) %>%
+  filter(channel != "CMS", channel != "DRL")
+
+elec_loc$radian_phi <- pi/180 * elec_loc$phi
+
+elec_loc <- elec_loc %>%
+  mutate(x = theta * cos(radian_phi),
+         y = theta * sin(radian_phi))
+
+Pz_elec <- c("A5")
+
+# merge coordinates with ERP data
+eeg_dat <- read_csv(here("data", "paper_two", "created_data", "erp_mast.csv"))
+eeg_dat$B28[eeg_dat$pid == 206201823 & eeg_dat$block == "Pos_Inc"] <- NA
+eeg_dat$A5[eeg_dat$pid == 206201843 & eeg_dat$block == "Neg_Watch"] <- NA
+eeg_dat_long <- pivot_longer(eeg_dat, cols = A1:EXG2, names_to = "electrode", values_to = "amplitude")
+eeg_dat_loc <- left_join(eeg_dat_long, elec_loc, by = c("electrode" = "channel"))
+topo_elec <- c(paste0("A", 1:32), paste0("B", 1:32))
+
+# late_tmp <- eeg_dat_loc %>%
+#   filter(!is.na(amplitude), block %in% c("Neu_Watch", "Pos_Watch", "Neg_Watch"),
+#          electrode %in% topo_elec, between(ms, 800, 2000)) %>%
+#   group_by(block, electrode, x, y) %>%
+#   dplyr::summarize(amplitude = mean(amplitude, na.rm = TRUE)) %>%
+#   mutate(highlight = if_else(electrode %in% Pz_elec, "white", "black"),
+#          block = if_else(block == "Neu_Watch", "Neutral",
+#                          if_else(block == "Pos_Watch", "Positive", "Negative")),
+#          block = factor(block, levels = c("Neutral", "Positive", "Negative")),
+#          comp = "Late")
+
+watch_topos <- eeg_dat_loc %>%
+  filter(!is.na(amplitude), block %in% c("Neu_Watch", "Pos_Watch", "Neg_Watch"),
+         electrode %in% topo_elec, between(ms, 1000, 1300)) %>%
+  group_by(block, electrode, x, y) %>%
+  dplyr::summarize(amplitude = mean(amplitude, na.rm = TRUE)) %>%
+  mutate(highlight = if_else(electrode %in% Pz_elec, "white", "black"),
+         block = if_else(block == "Neu_Watch", "Neutral",
+                         if_else(block == "Pos_Watch", "Positive", "Negative")),
+         block = factor(block, levels = c("Neutral", "Positive", "Negative"))) %>%
+  ggplot(., aes(x = x, y = y, fill = amplitude)) +
+  stat_scalpmap() +
+  geom_mask(scale_fac = 1.7) +
+  geom_head() +
+  geom_channels(aes(color = highlight), size = 0.125) +
+  scale_fill_viridis_c(limits = c(-4, 4), oob = scales::squish) +
+  scale_color_manual(breaks = c("black", "white"),
+                     values = c("black", "white"),
+                     guide = FALSE) +
+  labs(fill = expression(paste("Amplitude (", mu,"V)"))) +
+  coord_equal() +
+  theme_void() +
+  facet_wrap(~ block, ncol = 3) +
+  theme(strip.text.x = element_text(size = 12, vjust = 1),
+        strip.text.y = element_text(size = 12, angle = 90))
+
+watch_lpp <- eeg_dat %>%
+  filter(block %in% c("Neu_Watch", "Pos_Watch", "Neg_Watch")) %>%
+  select(all_of(Pz_elec),  block:prop_trials) %>%
+  filter(ms <= 1750) %>%
+  pivot_longer(., cols = Pz_elec, names_to = "electrode", values_to = "mv") %>%
+  group_by(block, ms) %>%
+  dplyr::summarize(mv = mean(mv, na.rm = TRUE)) %>%
+  ggplot(., aes(ms, mv, color = block)) +
+  geom_line(size = 1.1) +
+  geom_vline(xintercept = 0, linetype = "dashed") +
+  geom_vline(xintercept = c(1000, 1300), linetype = "solid", size = 1.05) +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  annotate("rect", xmin = 1000, xmax = 1300, ymin = -Inf, ymax = Inf, alpha = .2) +
+  labs(x = "Time (ms)",
+       y = expression(paste("Amplitude (",mu,"V)"))) +
+  #      title = paste("Average LPP Waveforms")) +
+  theme_classic() +
+  theme(axis.title = element_text(size = 12),
+        axis.text = element_text(size = 12),
+        legend.title = element_text(size = 12),
+        legend.text = element_text(size = 12),
+        legend.key.size = unit(1.5, "line"),
+        plot.title = element_text(hjust = 0.5),
+        title = element_text(size = 12)) +
+  scale_color_manual(values = c("red", "blue", "green"),
+                     name = "Block",
+                     breaks = c("Neg_Watch", "Pos_Watch", "Neu_Watch"),
+                     labels = c("Negative Watch", "Positive Watch", "Neutral Watch"))
+
+watch_topos / watch_lpp + plot_layout(widths = 2)
+
+ggsave("images/paper_2/front_lpp_watch_topos.png")
