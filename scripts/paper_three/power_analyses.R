125cdabd7aee9dd88a9daa3e4fb473c6c6125813
Ian Kahrilas <ijk60068@gmail.com>
2020-08-17 20:35:18 -0500

    Preliminary power analyses.

--- scripts/paper_three/power_analyses.R
+++ scripts/paper_three/power_analyses.R
@@ -11,7 +11,11 @@ pwr.r.test(n = 132,
 r
 plot(r)
 ## relation between depression and emotion reactivity (N = 107) with 3 repeated
-## measures for each participant (n - 1 = 320)
-wp.rmanova(n = 320, ng = 107, nm = 3, f = .48, nscor = 1,
+## measures for each participant (n = 321)
+wp.rmanova(n = 321, ng = 107, nm = 3, f = .48, nscor = 1,
            alpha = 0.05, power = NULL, type = 2)
 
+## relation between headspace use and emotion reactivity (N = 55) with 6 repeated measures
+## for each participant (n = 330)
+wp.rmanova(n = 330, ng = 55, nm = 6, f = .46, nscor = 1,
+           alpha = 0.05, power = NULL, type = 2)
