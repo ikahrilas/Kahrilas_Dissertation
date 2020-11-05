7b1a584087c075d444eba7d74c2de36d96817ef1
Ian Kahrilas <ijk60068@gmail.com>
2020-11-05 10:30:20 -0600

    Script for Hassan's Mulcahy project.

---
+++ scripts/paper_three/mulcahy script.R
@@ -0,0 +1,70 @@
+# Reading in data and analyses for Hassan's Mulcahy project
+
+## load packages
+library(tidyverse)
+library(readxl)
+library(MBESS)
+library(naniar)
+
+## read in data
+dat <- read_excel("data/paper_three/headspace_questionnaire_data.xlsx") %>%
+  select("Initials",
+         "Cohort",
+         "Code Activation Date",
+         "Group",
+         contains(c("T1", "T2", "T3"))) %>%
+  filter(Group != "WL+")
+
+## race code
+dat_hs <- dat %>%
+  rowwise() %>%
+  mutate(race = if_else(sum(`Q9African-American_T1`,
+                            Q9AmericanIndianorAlaskaNat_T1,
+                            `Q9Asian-American_T1`,
+                            Q9NativeHawaiianorOtherPaci_T1,
+                            Q9HispanicorLatino_T1,
+                            `Q9Non-HispanicCaucasian/Whi_T1`,
+                            Q9Other_T1
+  ) >=2, "Multi-Racial", if_else(
+    `Q9African-American_T1` == 1, "African American", if_else(
+      Q9AmericanIndianorAlaskaNat_T1 == 1, "American Indian or Alaska Native", if_else(
+        `Q9Asian-American_T1` == 1, "Asian American", if_else(
+          Q9NativeHawaiianorOtherPaci_T1 == 1, "Native Hawaiian or Other Pacific Islander", if_else(
+            Q9HispanicorLatino_T1 == 1, "Hispanic or Latinx", if_else(
+              Q9Other_T1 == 1, Q9OtherOther_T1, "Caucasian"
+            )
+          )
+        )
+      )
+    )
+  )
+  ),
+  race = if_else(race == "666", "Other", race)
+  ) %>%
+  rename(pid = StudyID_T1)
+
+dat_hs <- dat_hs %>%
+  select("Initials",
+         "Cohort",
+         "Group",
+         "race",
+         contains(c("Gender", "age", "PHQ", "MASQ", "SBI", "PSWQ", "PANAS", "BSI")),
+         -contains("Language")) %>%
+  rename("age" = "Age_T1",
+         "gender" = "Gender_T1")
+
+## replace 666 and 999 with NAs
+dat_hs <- map_df(dat_hs, ~ {
+  na_if(.x, 999) %>%
+    na_if(666)
+})
+
+## clean up variable names
+names(dat_hs) <- str_remove_all(names(dat_hs), "...160")
+names(dat_hs) <- str_remove_all(names(dat_hs), "...163")
+names(dat_hs) <- str_remove_all(names(dat_hs), "...170")
+names(dat_hs) <- str_remove_all(names(dat_hs), "...173")
+
+## convert MASQ8 to numeric
+dat_hs$MASQ8_T1 <- as.numeric(dat_hs$MASQ8_T1)
+
