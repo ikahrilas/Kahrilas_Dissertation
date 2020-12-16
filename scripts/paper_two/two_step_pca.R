81e822da244d26d9349aeda6d47d6e037b9759c0
Ian Kahrilas <ijk60068@gmail.com>
2020-12-16 15:11:35 -0600

    experimenting with different plots

--- scripts/paper_two/two_step_pca.R
+++ scripts/paper_two/two_step_pca.R
@@ -6,6 +6,9 @@ library(here)
 library(factoextra)
 library(sjstats)
 library(psych)
+library(devtools)
+library(eegUtils) # remotes::install_github("craddm/eegUtils")
+library(patchwork)
 
 # read in average referenced data set
 ## average referenced data
@@ -118,14 +121,73 @@ temp_loadings_plot
 ## based on these loading plots, should retain 1-3, 5, 6, 9, 10, 11, 12-32, 34, 36-38, 41
 
 # this code extracts the covariance loadings for each rotated factor along with its corresponding timepoint
-# in a dataframe, such that it can be merged with the raw data to derive factor scores.
-loadings_lst <- map(paste0("RC", 1:43), ~ {
-  select(cov_loadings_df, .x, ms)
-})
+# in a data frame, such that it can be merged with the raw data to derive factor scores.
+## name of electrodes to be used in tidyeval
 elec_vec <- c(paste0("A", 1:32), paste0("B", 1:32), "EXG1", "EXG2")
+## covariance loading by component extraction
+loadings_lst <- map(paste0("RC", 1:43), ~ {
+  select(cov_loadings_df, all_of(.x), ms) %>%
+    rename("component" = .x)
+})
+## 43 data frames with weighted ERPs for each component
+weighted_dfs_lst <- map(loadings_lst, ~ {
+  full_join(filter(dat, ms < 2000), all_of(.x), by = "ms") %>%
+    mutate(across(.cols = all_of(elec_vec), .fns = ~ all_of(.x) * component))
+})
 
-full_join(filter(dat, ms < 2000), loadings_lst[[2]], by = "ms") %>%
-  mutate(across(.cols = elec_vec, .fns = ~ .x * RC2))
+# read in EEG coordinate data
+elec_loc <- read_csv(here("data", "paper_two", "Equidistant Layout.csv"))
+elec_loc <- elec_loc %>%
+  rename("channel" = `channel name`) %>%
+  filter(channel != "CMS", channel != "DRL") %>%
+  select(-number)
+
+elec_loc$radian_phi <- pi/180 * elec_loc$phi
+
+elec_loc <- elec_loc %>%
+  mutate(x = theta * cos(radian_phi),
+         y = theta * sin(radian_phi))
+
+# start writing code for topo plotting
+long_loading <- pivot_longer(weighted_dfs_lst[[3]],
+                             cols = all_of(elec_vec),
+                             names_to = "elec",
+                             values_to = "mv")
+
+long_loading_loc <- left_join(long_loading, elec_loc, by = c("elec" = "channel"))
+
+# try plotting
+long_loading_loc %>%
+  filter(!is.na(mv), block %in% c("Neu_Watch", "Pos_Watch", "Neg_Watch")) %>%
+  group_by(block, elec, x, y) %>%
+  dplyr::summarize(mv = mean(mv, na.rm = TRUE)) %>%
+  mutate(block = if_else(block == "Neu_Watch", "Neutral",
+                         if_else(block == "Pos_Watch", "Positive", "Negative")),
+         block = factor(block, levels = c("Neutral", "Positive", "Negative"))) %>%
+  ggplot(., aes(x = x, y = y, fill = mv)) +
+  stat_scalpmap() +
+  geom_mask(scale_fac = 1.7) +
+  geom_head() +
+  geom_channels(size = 0.125) +
+  scale_fill_viridis_c(limits = c(-1, 1), oob = scales::squish) +
+  scale_color_manual(breaks = c("black", "white"),
+                     values = c("black", "white"),
+                     guide = FALSE) +
+  labs(fill = expression(paste("Weighted ", mu,"V"))) +
+  coord_equal() +
+  theme_void() +
+  facet_wrap(~ block, ncol = 3) +
+  theme(strip.text.x = element_text(size = 12, vjust = 1),
+        strip.text.y = element_text(size = 12, angle = 90))
+
+
+plot_butterfly(rename(long_loading_loc, "time" = "ms", "electrode" = "elec", "amplitude" = "mv"))
+
+
+erp_scalp(rename(long_loading_loc, "time" = "ms", "electrode" = "elec", "amplitude" = "mv"),
+          montage = "biosemi64alpha",
+          size = 0.5,
+          color = as.factor(.data$block))
 
 
 save.image(file = paste0("scripts/paper_two/", Sys.Date(), "_two_step_pca", ".RData"))
