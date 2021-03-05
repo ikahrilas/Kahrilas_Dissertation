b110ff6bdd4083d08301d06fee759b88430bc6b5
Ian Kahrilas <ijk60068@gmail.com>
2021-03-05 17:30:43 -0600

    Fix up correlation table to partition by block.

--- scripts/paper_two/correlation_table.R
+++ scripts/paper_two/correlation_table.R
@@ -7,36 +7,49 @@ library(magick)
 library(kableExtra)
 
 # load data
-dat <- read_csv("data/paper_two/created_data/temp_fac_score_dat_analyses2021-02-11.csv")
+cor_dat <- read_csv("data/paper_two/created_data/temp_fac_score_dat_analyses2021-02-11.csv")%>%
+  rename("381 ms Comp" = "RC2", # rename components
+         "740 ms Comp" = "RC3",
+         "124 ms Comp" = "RC5",
+         "162 ms Comp" = "RC11",
+         "Neg 259 ms Comp" = "RC12",
+         "Pos 259 ms Comp" = "pos_RC12")
 
-# rename components
-cor_dat <- dat %>%
-  rename("Early LPP" = "RC2",
-         "Late LPP" = "RC3",
-         "P125" = "RC5",
-         "N170" = "RC11",
-         "EPN" = "RC12",
-         "EPP" = "pos_RC12")
+glimpse(cor_dat)
 
 # create correlation matrix
+cor_list_fun <- function(block_type) {
 tab <- cor_dat %>%
-  select(`Early LPP`:valence,
+  filter(block == block_type) %>%
+  select(`381 ms Comp`:valence,
+         anticipating:reminiscing,
          pos_affectivity,
-         savoring_moment,
-         depression) %>%
-  relocate(P125, N170, EPN, EPP, `Early LPP`, `Late LPP`, everything()) %>%
-  rename("1. P125" = P125,
-         "2. N170" = N170,
-         "3. EPN" = EPN,
-         "4. EPP" = EPP,
-         "5. Early LPP" = `Early LPP`,
-         "6. Late LPP" = `Late LPP`,
+         neg_affectivity,
+         erq_reappraisal,
+         erq_suppression) %>%
+  relocate(`124 ms Comp`,
+           `162 ms Comp`,
+           `Neg 259 ms Comp`,
+           `Pos 259 ms Comp`,
+           `381 ms Comp`,
+           `740 ms Comp`,
+           everything()) %>%
+  rename("1. 124 ms Comp" = "124 ms Comp",
+         "2. 162 ms Comp" = "162 ms Comp",
+         "3. Neg 259 ms Comp" =  "Neg 259 ms Comp",
+         "4. Pos 259 ms Comp" = "Pos 259 ms Comp",
+         "5. 381 ms Comp" = "381 ms Comp",
+         "6. 740 ms Comp" = "740 ms Comp",
          "7. Arousal Ratings" = arousal,
          "8. Valence Ratings" = valence,
          "9. Difficulty Ratings" = difficulty,
          "10. PA" = pos_affectivity,
-         "11. StM" = savoring_moment,
-         "12. Depression" = depression)
+         "11. NA" = neg_affectivity,
+         "12. Ant" = anticipating,
+         "13. StM" = savoring_moment,
+         "14. Rem" = reminiscing,
+         "15. Reapp" = erq_reappraisal,
+         "16. Supp" = erq_suppression)
 
 #Compute correlation matrix
 x <- as.matrix(tab)
@@ -75,108 +88,47 @@ tab <- tab %>%
   map_df(., ~ {
     if_else(str_detect(.x, "-"), paste0("-", str_remove(.x, "-0")), .x)
   })
-colnames(tab) <- paste0(c(1:11), ".")
-save.image(file = paste0("data/paper_two/analyses/", Sys.Date(), "_correlation_table-data", ".RData"))
+colnames(tab) <- paste0(c(1:15), ".")
+
 tab %>%
-  mutate(" " = c("1. P125",
-                 "2. N170",
-                 "3. EPN",
-                 "4. EPP",
-                 "5. Early LPP",
-                 "6. Late LPP",
+  mutate(" " = c("1. 124 ms Comp",
+                 "2. 162 ms Comp",
+                 "3. Neg 259 ms Comp",
+                 "4. Pos 259 ms Comp",
+                 "5. 381 ms Comp",
+                 "6. 740 ms Comp",
                  "7. Arousal Ratings",
                  "8. Valence Ratings",
                  "9. Difficulty Ratings",
                  "10. PA",
-                 "11. StM",
-                 "12. Depression")) %>%
-  select(" ", everything()) %>%
-  ## create table
-  kable(., linesep = "", escape = FALSE, booktabs = TRUE, align = c("l", rep("r", 13)), caption = "(ref:corr-table)") %>%
+                 "11. NA",
+                 "12. Ant",
+                 "13. StM",
+                 "14. Rem",
+                 "15. Reapp",
+                 "16. Supp")) %>%
+  select(" ", everything())
+}
+
+corr_list <- map(unique(cor_dat$block), ~ cor_list_fun(.x))
+
+save.image(file = paste0("data/paper_two/analyses/", Sys.Date(), "_correlation_table-data", ".RData"))
+
+## create tables
+blocks <- c("Negative Increase", "Negative Decrease", "Negative Watch",
+            "Neutral Watch",
+            "Positive Watch", "Positive Decrease", "Positive Increase")
+
+map2(corr_list, blocks, ~ {
+.x %>%
+  kable(linesep = "", escape = FALSE, booktabs = TRUE, align = c("l", rep("r", 16)), caption = .y) %>%
   kable_styling(latex_options = "scale_down", font_size = 12) %>%
-  column_spec(1:12, width = "3em") %>%
+  column_spec(1:16, width = "3em") %>%
   footnote(general_title = "Note.",
            escape = FALSE,
-           general = "*$p < .05$. PA = positive affectivity, StM = savoring the moment. EEG components and behavioral ratings (i.e., arousal, valence, and difficulty) averaged across all regulation blocks.",
+           general = "*$p < .05$. PA = positive affectivity, NA = negative affectivity,
+           Ant = anticipating, StM = savoring the moment, Rem = reminiscing,
+           Reapp = reappraisal, Supp = suppression.",
            threeparttable = TRUE,
            footnote_as_chunk = TRUE)
-
-# table with EEG components and behavioral ratings conditioned on regulation block
-cor_block_tab <- cor_dat %>%
-  select(block,
-         `Early LPP`:valence,
-         pos_affectivity,
-         savoring_moment,
-         depression) %>%
-  relocate(P125, N170, EPN, EPP, `Early LPP`, `Late LPP`, everything()) %>%
-  pivot_wider(names_from = block,
-              values_from = c(P125:`Late LPP`, arousal:valence),
-              names_sep = "_") %>%
-  relocate(P125_Neg_Inc:valence_Pos_Inc, savoring_moment, pos_affectivity, depression)
-
-colnames(cor_block_tab) <- tools::toTitleCase(str_replace_all(colnames(cor_block_tab), "_", " "))
-
-colnames(cor_block_tab) <- paste0(c(1:length(colnames(cor_block_tab))), ". ", colnames(cor_block_tab))
-
-colnames(cor_block_tab) <- str_replace(colnames(cor_block_tab), "64. Savoring Moment", "64. StM")
-
-colnames(cor_block_tab) <- str_replace(colnames(cor_block_tab), "65. Pos Affectivity", "65. PA")
-
-col_names_for_table <- colnames(cor_block_tab)
-
-#Compute correlation matrix
-x <- as.matrix(cor_block_tab)
-correlation_matrix <- Hmisc::rcorr(x)
-R <- correlation_matrix$r # Matrix of correlation coeficients
-p <- correlation_matrix$P # Matrix of p-value
-
-## Define notions for significance levels; spacing is important.
-mystars <- ifelse(p < .05, "*", " ")
-
-## trunctuate the correlation matrix to two decimal
-R <- format(round(cbind(rep(-1.11, ncol(x)), R), 2))[,-1]
-
-## build a new matrix that includes the correlations with their appropriate stars
-Rnew <- matrix(paste(R, mystars, sep=""), ncol=ncol(x))
-diag(Rnew) <- paste(diag(R), " ", sep="")
-rownames(Rnew) <- colnames(x)
-colnames(Rnew) <- paste(colnames(x), "", sep="")
-
-## remove upper triangle of correlation matrix
-Rnew <- as.matrix(Rnew)
-Rnew[upper.tri(Rnew, diag = TRUE)] <- ""
-Rnew <- as.data.frame(Rnew)
-
-## remove last column and return the correlation matrix
-cor_block_tab <- cbind(Rnew[1:length(Rnew)-1])
-
-## clean it up and prepare for latex rendering
-cor_block_tab <- cor_block_tab %>%
-  as.data.frame() %>%
-  map_df(., ~ {
-    as.character(.x) %>%
-      str_trim() %>%
-      str_remove("^0+")
-  }) %>%
-  map_df(., ~ {
-    if_else(str_detect(.x, "-"), paste0("-", str_remove(.x, "-0")), .x)
-  })
-
-colnames(cor_block_tab) <- paste0(c(1:65), ".")
-
-save.image(file = paste0("data/paper_two/analyses/", Sys.Date(), "block_correlation_table-data", ".RData"))
-
-cor_block_tab %>%
-  mutate(" " = col_names_for_table) %>%
-  select(" ", everything()) %>%
-  ## create table
-  kable(., format = "html",  linesep = "", escape = FALSE, booktabs = TRUE, align = c("l", rep("r", 13)), caption = "(ref:corr-block-table)") %>%
-  kable_styling(latex_options = "scale_down", font_size = 12) %>%
-  column_spec(2:66, width = "3em") %>%
-  footnote(general_title = "Note.",
-           escape = FALSE,
-           general = "*$p < .05$. PA = positive affectivity, StM = savoring the moment",
-           threeparttable = TRUE,
-           footnote_as_chunk = TRUE)
-
-# create correlation plot just highlighting the strongest relations among variables
+})
