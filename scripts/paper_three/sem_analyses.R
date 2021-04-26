d200bb127ca63c1ba11ab5d241a01a4a9c7f5982
Ian Kahrilas <ijk60068@gmail.com>
2021-04-26 11:56:50 -0500

    add 134 ms analyses

--- scripts/paper_three/sem_analyses.R
+++ scripts/paper_three/sem_analyses.R
@@ -1090,3 +1090,271 @@ chi_rc3_anxapp_meas <- lavTestLRT(fit_meas_rc3, fit_anxapp_rc3)
 
 # save all analyses to work space
 save.image("data/paper_three/sem_analyses_results.RData")
+
+
+
+
+
+################
+## -- RC5 -- ##
+################
+RC5_meas_mod <- '
+# neural responsivity factor
+  NR =~ RC5_Neu_Watch + RC5_Pos_Watch + RC5_Neg_Watch
+# internalizing measurement model with MASQ subscales
+  INT =~ masq_aa + masq_pa + pswq_total
+# orthogonal model
+  NR ~~ 0*INT
+'
+
+fit_meas_RC5 <- cfa(RC5_meas_mod,
+                    data = dat,
+                    estimator = "MLR",
+                    missing = "ML",
+                    std.lv = TRUE)
+
+summary(fit_meas_RC5, fit.measures = TRUE, standardized = TRUE)
+
+RC5_meas_params <- tidy(fit_meas_RC5)
+
+RC5_meas_fit <- fitMeasures(fit_meas_RC5, c("chisq.scaled",
+                                            "df",
+                                            "rmsea.scaled",
+                                            "srmr",
+                                            "cfi.scaled",
+                                            "nnfi.scaled",
+                                            "aic",
+                                            "bic"))
+
+semPaths(fit_meas_RC5,
+         what = "diagram",
+         whatLabels = "est",
+         style = "lisrel",
+         nCharNodes = 0,
+         intercepts = FALSE,
+         sizeMan = 8,
+         sizeLat = 10,
+         nodeLabels = c("NEU", "POS", "NEG",
+                        "AA", "PA", "WOR",
+                        "NR", "INT"))
+
+# define eci model with RC5
+RC5_eci_mod <- '
+# neural responsivity factor
+  NR =~ RC5_Neu_Watch + RC5_Pos_Watch + RC5_Neg_Watch
+# internalizing measurement model with MASQ subscales
+  INT =~ masq_aa + masq_pa + pswq_total
+# orthogonal model
+  NR ~~ 0*INT
+# residual covariances (constrained to equality)
+  masq_pa ~~ RC5_Pos_Watch + RC5_Neg_Watch
+'
+
+fit_eci_RC5 <- cfa(RC5_eci_mod,
+                   data = dat,
+                   estimator = "MLR",
+                   missing = "ML",
+                   std.lv = TRUE)
+
+summary(fit_eci_RC5, fit.measures = TRUE, standardized = TRUE)
+
+RC5_eci_params <- tidy(fit_eci_RC5)
+
+RC5_eci_fit <- fitMeasures(fit_eci_RC5, c("chisq.scaled",
+                                          "df",
+                                          "rmsea.scaled",
+                                          "srmr",
+                                          "cfi.scaled",
+                                          "nnfi.scaled",
+                                          "aic",
+                                          "bic"))
+
+semPaths(fit_eci_RC5,
+         what = "diagram",
+         whatLabels = "est",
+         style = "lisrel",
+         nCharNodes = 0,
+         intercepts = FALSE,
+         sizeMan = 8,
+         sizeLat = 10,
+         nodeLabels = c("NEU", "POS", "NEG",
+                        "AA", "PA", "WOR",
+                        "NR", "INT"))
+
+## model comparison test with measurement model
+chi_RC5_eci_meas <- lavTestLRT(fit_meas_RC5, fit_eci_RC5)
+### comparison model does NOT fit the data significantly better
+
+# define neural reactivity model with RC5
+RC5_nr_mod <- '
+# neural responsivity factor
+  NR =~ RC5_Neu_Watch + RC5_Pos_Watch + RC5_Neg_Watch
+# internalizing measurement model with MASQ subscales
+  INT =~ masq_aa + masq_pa + pswq_total
+# no correlation with internalizing factor but correlated with masq_pa
+  NR ~~ 0*INT + masq_pa
+'
+
+fit_nr_RC5 <- cfa(RC5_nr_mod,
+                  data = dat,
+                  estimator = "MLR",
+                  missing = "ML",
+                  std.lv = TRUE)
+
+summary(fit_nr_RC5, fit.measures = TRUE, standardized = TRUE)
+
+RC5_nr_params <- tidy(fit_nr_RC5)
+
+RC5_nr_fit <- fitMeasures(fit_nr_RC5, c("chisq.scaled",
+                                        "df",
+                                        "rmsea.scaled",
+                                        "srmr",
+                                        "cfi.scaled",
+                                        "nnfi.scaled",
+                                        "aic",
+                                        "bic"))
+
+## model comparison test with measurement model
+chisq_RC5_nr_meas <- lavTestLRT(fit_meas_RC5, fit_nr_RC5)
+### comparison model does NOT fit the data significantly better
+
+# define internalizing model with RC5
+RC5_int_mod <- '
+# neural responsivity factor
+  NR =~ RC5_Neu_Watch + RC5_Pos_Watch + RC5_Neg_Watch
+# internalizing measurement model with MASQ subscales
+  INT =~ masq_aa + masq_pa + pswq_total
+# no correlation with internalizing factor but correlated with masq_pa
+  NR ~~ 0*INT
+# estimate parameters among pos and neg watch and internalizing factor constrained to equality
+INT ~~ RC5_Pos_Watch + RC5_Neg_Watch
+'
+
+fit_int_RC5 <- cfa(RC5_int_mod,
+                   data = dat,
+                   estimator = "MLR",
+                   missing = "ML",
+                   std.lv = TRUE)
+
+summary(fit_int_RC5, fit.measures = TRUE, standardized = TRUE)
+
+RC5_int_params <- tidy(fit_int_RC5)
+
+RC5_int_fit <- fitMeasures(fit_int_RC5, c("chisq.scaled",
+                                          "df",
+                                          "rmsea.scaled",
+                                          "srmr",
+                                          "cfi.scaled",
+                                          "nnfi.scaled",
+                                          "aic",
+                                          "bic"))
+
+semPaths(fit_int_RC5,
+         what = "diagram",
+         whatLabels = "est",
+         style = "lisrel",
+         nCharNodes = 0,
+         intercepts = FALSE,
+         sizeMan = 8,
+         sizeLat = 10,
+         nodeLabels = c("NEU", "POS", "NEG",
+                        "AA", "PA", "WOR",
+                        "NR", "INT"))
+
+## model comparison test with measurement model
+chisq_RC5_int_meas <- lavTestLRT(fit_meas_RC5, fit_int_RC5)
+### comparison model does NOT fit the data significantly better
+
+# define anxiety model with RC5
+RC5_anx_mod <- '
+# neural responsivity factor
+  NR =~ RC5_Neu_Watch + RC5_Pos_Watch + RC5_Neg_Watch
+# internalNRizing measurement model with MASQ subscales
+  INT =~ masq_aa + masq_pa + pswq_total
+# no correlation with internalizing factor but correlated with masq_pa
+  NR ~~ 0*INT
+# estimate parameters among pos and neg watch and internalizing factor constrained to equality
+masq_aa ~~ RC5_Pos_Watch + RC5_Neg_Watch
+'
+
+fit_anx_RC5 <- cfa(RC5_anx_mod,
+                   data = dat,
+                   estimator = "MLR",
+                   missing = "ML",
+                   std.lv = TRUE)
+
+summary(fit_anx_RC5, fit.measures = TRUE, standardized = TRUE)
+
+RC5_anx_params <- tidy(fit_anx_RC5)
+
+RC5_anx_fit <- fitMeasures(fit_anx_RC5, c("chisq.scaled",
+                                          "df",
+                                          "rmsea.scaled",
+                                          "srmr",
+                                          "cfi.scaled",
+                                          "nnfi.scaled",
+                                          "aic",
+                                          "bic"))
+
+semPaths(fit_anx_RC5,
+         what = "diagram",
+         whatLabels = "est",
+         style = "lisrel",
+         nCharNodes = 0,
+         intercepts = FALSE,
+         sizeMan = 8,
+         sizeLat = 10,
+         nodeLabels = c("NEU", "POS", "NEG",
+                        "AA", "PA", "WOR",
+                        "NR", "INT"))
+
+## model comparison test with measurement model
+chi_RC5_anx_meas <- lavTestLRT(fit_meas_RC5, fit_anx_RC5)
+### comparison model does NOT fit the data significantly better
+
+# define anxiety model with RC5
+RC5_anxapp_mod <- '
+# neural responsivity factor
+  NR =~ RC5_Neu_Watch + RC5_Pos_Watch + RC5_Neg_Watch
+# internalNRizing measurement model with MASQ subscales
+  INT =~ masq_aa + masq_pa + pswq_total
+# no correlation with internalizing factor but correlated with masq_pa
+  NR ~~ 0*INT
+# estimate parameters among pos and neg watch and internalizing factor constrained to equality
+pswq_total ~~ RC5_Pos_Watch + RC5_Neg_Watch
+'
+
+fit_anxapp_RC5 <- cfa(RC5_anxapp_mod,
+                      data = dat,
+                      estimator = "MLR",
+                      missing = "ML",
+                      std.lv = TRUE)
+
+summary(fit_anxapp_RC5, fit.measures = TRUE, standardized = TRUE)
+
+RC5_anxapp_params <- tidy(fit_anxapp_RC5)
+
+RC5_anxapp_fit <- fitMeasures(fit_anxapp_RC5, c("chisq.scaled",
+                                                "df",
+                                                "rmsea.scaled",
+                                                "srmr",
+                                                "cfi.scaled",
+                                                "nnfi.scaled",
+                                                "aic",
+                                                "bic"))
+
+semPaths(fit_anxapp_RC5,
+         what = "diagram",
+         whatLabels = "est",
+         style = "lisrel",
+         nCharNodes = 0,
+         intercepts = FALSE,
+         sizeMan = 8,
+         sizeLat = 10,
+         nodeLabels = c("NEU", "POS", "NEG",
+                        "AA", "PA", "WOR",
+                        "NR", "INT"))
+
+## model comparison test with measurement model
+chi_nrc8_anxapp_meas <- lavTestLRT(fit_meas_RC5, fit_anxapp_RC5)
+### comparison model does NOT fit the data significantly better
